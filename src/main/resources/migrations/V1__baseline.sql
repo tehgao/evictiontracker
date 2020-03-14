804604f2e1b8c89d86cee5e358f016b0b0a4be22
Alvin Gao <gaovinal@gmail.com>
2020-03-13 23:24:05 -0400

    Fixed issues with flyway

--- src/main/resources/migrations/V1__baseline.sql
+++ src/main/resources/migrations/V1__baseline.sql
@@ -1,151 +1,130 @@
--- Adminer 4.7.6 PostgreSQL dump
-
-DROP TABLE IF EXISTS "address_entity";
-CREATE TABLE "public"."address_entity"
+create table public.address_entity
 (
-    "id"                       uuid NOT NULL,
-    "created_date"             timestamp,
-    "last_modified_date"       timestamp,
-    "city"                     character varying(255),
-    "latitude"                 numeric(19, 2),
-    "longitude"                numeric(19, 2),
-    "neighborhood"             character varying(255),
-    "state"                    character varying(255),
-    "street_address"           character varying(255),
-    "street_address_secondary" character varying(255),
-    "zip_code"                 character varying(255),
-    "created_by_user_id"       character varying(255),
-    "last_modified_by_user_id" character varying(255),
-    CONSTRAINT "address_entity_pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fkc6gw42h0frnp52se4a1w1xef9" FOREIGN KEY (created_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE,
-    CONSTRAINT "fkgorye8dxkkxqkt76vff5mwfpk" FOREIGN KEY (last_modified_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "attorney_entity";
-CREATE TABLE "public"."attorney_entity"
+    id                       uuid not null,
+    created_date             timestamp,
+    last_modified_date       timestamp,
+    city                     varchar(255),
+    latitude                 numeric(19, 2),
+    longitude                numeric(19, 2),
+    neighborhood             varchar(255),
+    state                    varchar(255),
+    street_address           varchar(255),
+    street_address_secondary varchar(255),
+    zip_code                 varchar(255),
+    created_by_user_id       varchar(255),
+    last_modified_by_user_id varchar(255),
+    primary key (id)
+);
+create table public.attorney_entity
 (
-    "id"                       uuid NOT NULL,
-    "created_date"             timestamp,
-    "last_modified_date"       timestamp,
-    "name"                     character varying(255),
-    "created_by_user_id"       character varying(255),
-    "last_modified_by_user_id" character varying(255),
-    CONSTRAINT "attorney_entity_pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk39u5gdi81u52py4j33lc9o25r" FOREIGN KEY (last_modified_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE,
-    CONSTRAINT "fkg5p8wpsalo8177xibhofmg0so" FOREIGN KEY (created_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "case_entity";
-CREATE TABLE "public"."case_entity"
+    id                       uuid not null,
+    created_date             timestamp,
+    last_modified_date       timestamp,
+    name                     varchar(255),
+    created_by_user_id       varchar(255),
+    last_modified_by_user_id varchar(255),
+    primary key (id)
+);
+create table public.case_entity
 (
-    "id"                       uuid NOT NULL,
-    "created_date"             timestamp,
-    "last_modified_date"       timestamp,
-    "case_number"              character varying(255),
-    "file_date"                date,
-    "created_by_user_id"       character varying(255),
-    "last_modified_by_user_id" character varying(255),
-    CONSTRAINT "case_entity_pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "uk_bcn58de4xlby2456wpf30c2f" UNIQUE ("case_number"),
-    CONSTRAINT "fk9os7lxvix0bdf9xs7k969owxl" FOREIGN KEY (created_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE,
-    CONSTRAINT "fko4pk32c9ebum5vjfra2ipt024" FOREIGN KEY (last_modified_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "case_entity_defendants";
-CREATE TABLE "public"."case_entity_defendants"
+    id                       uuid not null,
+    created_date             timestamp,
+    last_modified_date       timestamp,
+    case_number              varchar(255),
+    file_date                date,
+    created_by_user_id       varchar(255),
+    last_modified_by_user_id varchar(255),
+    primary key (id)
+);
+create table public.case_entity_defendants
 (
-    "is_defendant_of_id" uuid NOT NULL,
-    "defendants_id"      uuid NOT NULL,
-    CONSTRAINT "fk74ffhi1knvlevihy434ttqw9u" FOREIGN KEY (defendants_id) REFERENCES party_entity (id) NOT DEFERRABLE,
-    CONSTRAINT "fk89jbpcgkg8sjmodtywdegw8l4" FOREIGN KEY (is_defendant_of_id) REFERENCES case_entity (id) NOT DEFERRABLE
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "case_entity_events";
-CREATE TABLE "public"."case_entity_events"
+    is_defendant_of_id uuid not null,
+    defendants_id      uuid not null
+);
+create table public.case_entity_events
 (
-    "case_entity_id" uuid NOT NULL,
-    "events_id"      uuid NOT NULL,
-    CONSTRAINT "uk_jaokkwoye39uulvyaon48j983" UNIQUE ("events_id"),
-    CONSTRAINT "fk58poo21r6m9iubjys156reb15" FOREIGN KEY (events_id) REFERENCES event_entity (id) NOT DEFERRABLE,
-    CONSTRAINT "fkfbv6b9413lpv4xvxpv0fqhhvj" FOREIGN KEY (case_entity_id) REFERENCES case_entity (id) NOT DEFERRABLE
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "case_entity_plaintiffs";
-CREATE TABLE "public"."case_entity_plaintiffs"
+    case_entity_id uuid not null,
+    events_id      uuid not null
+);
+create table public.case_entity_plaintiffs
 (
-    "is_plaintiff_of_id" uuid NOT NULL,
-    "plaintiffs_id"      uuid NOT NULL,
-    CONSTRAINT "fkgob6c5vq2ljo4ubydtuxgxu3b" FOREIGN KEY (is_plaintiff_of_id) REFERENCES case_entity (id) NOT DEFERRABLE,
-    CONSTRAINT "fkj4gj04c6jm7ntxhs9yccbqlhk" FOREIGN KEY (plaintiffs_id) REFERENCES party_entity (id) NOT DEFERRABLE
-) WITH (oids = false);
-
-DROP TABLE IF EXISTS "event_entity";
-CREATE TABLE "public"."event_entity"
+    is_plaintiff_of_id uuid not null,
+    plaintiffs_id      uuid not null
+);
+create table public.event_entity
 (
-    "id"                       uuid    NOT NULL,
-    "created_date"             timestamp,
-    "last_modified_date"       timestamp,
-    "date_time"                timestamp,
-    "event_type"               integer,
-    "pro_se"                   boolean NOT NULL,
-    "created_by_user_id"       character varying(255),
-    "last_modified_by_user_id" character varying(255),
-    CONSTRAINT "event_entity_pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fkep2o90etyl8tuuqhir8wp47qs" FOREIGN KEY (created_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE,
-    CONSTRAINT "fktis6v9gx6m48w6vmw5ix0obgm" FOREIGN KEY (last_modified_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "evictions_user";
-CREATE TABLE "public"."evictions_user"
+    id                       uuid    not null,
+    created_date             timestamp,
+    last_modified_date       timestamp,
+    date_time                timestamp,
+    event_type               int4,
+    pro_se                   boolean not null,
+    created_by_user_id       varchar(255),
+    last_modified_by_user_id varchar(255),
+    primary key (id)
+);
+create table public.evictions_user
 (
-    "user_id" character varying(255) NOT NULL,
-    CONSTRAINT "evictions_user_pkey" PRIMARY KEY ("user_id")
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "flyway_schema_history";
-CREATE TABLE "public"."flyway_schema_history"
+    user_id varchar(255) not null,
+    primary key (user_id)
+);
+create table public.party_entity
 (
-    "installed_rank" integer                 NOT NULL,
-    "version"        character varying(50),
-    "description"    character varying(200)  NOT NULL,
-    "type"           character varying(20)   NOT NULL,
-    "script"         character varying(1000) NOT NULL,
-    "checksum"       integer,
-    "installed_by"   character varying(100)  NOT NULL,
-    "installed_on"   timestamp DEFAULT now() NOT NULL,
-    "execution_time" integer                 NOT NULL,
-    "success"        boolean                 NOT NULL,
-    CONSTRAINT "flyway_schema_history_pk" PRIMARY KEY ("installed_rank")
-) WITH (oids = false);
-
-CREATE INDEX "flyway_schema_history_s_idx" ON "public"."flyway_schema_history" USING btree ("success");
-
-
-DROP TABLE IF EXISTS "party_entity";
-CREATE TABLE "public"."party_entity"
-(
-    "id"                       uuid NOT NULL,
-    "created_date"             timestamp,
-    "last_modified_date"       timestamp,
-    "name"                     character varying(255),
-    "created_by_user_id"       character varying(255),
-    "last_modified_by_user_id" character varying(255),
-    "address_id"               uuid,
-    "attorney_id"              uuid,
-    CONSTRAINT "party_entity_pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "uk_kh29n4a2lp4y8u0w8119hl02y" UNIQUE ("name"),
-    CONSTRAINT "fk3o4ifiyyy52vbkd6mjuds1ruh" FOREIGN KEY (created_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE,
-    CONSTRAINT "fkbnnr3uf2tr20juepomwqrtv8f" FOREIGN KEY (last_modified_by_user_id) REFERENCES evictions_user (user_id) NOT DEFERRABLE,
-    CONSTRAINT "fkpcb1kvwk2hsa4ik8aw6t0ib3h" FOREIGN KEY (attorney_id) REFERENCES attorney_entity (id) NOT DEFERRABLE,
-    CONSTRAINT "fkqd32hd4df92rwtcfceewu7ro9" FOREIGN KEY (address_id) REFERENCES address_entity (id) NOT DEFERRABLE
-) WITH (oids = false);
-
-
--- 2020-03-11 01:28:19.559444+00
+    id                       uuid not null,
+    created_date             timestamp,
+    last_modified_date       timestamp,
+    name                     varchar(255),
+    created_by_user_id       varchar(255),
+    last_modified_by_user_id varchar(255),
+    address_id               uuid,
+    attorney_id              uuid,
+    primary key (id)
+);
+alter table public.case_entity
+    drop constraint if exists UK_bcn58de4xlby2456wpf30c2f;
+alter table public.case_entity
+    add constraint UK_bcn58de4xlby2456wpf30c2f unique (case_number);
+alter table public.case_entity_events
+    drop constraint if exists UK_jaokkwoye39uulvyaon48j983;
+alter table public.case_entity_events
+    add constraint UK_jaokkwoye39uulvyaon48j983 unique (events_id);
+alter table public.party_entity
+    drop constraint if exists UK_kh29n4a2lp4y8u0w8119hl02y;
+alter table public.party_entity
+    add constraint UK_kh29n4a2lp4y8u0w8119hl02y unique (name);
+alter table public.address_entity
+    add constraint FKc6gw42h0frnp52se4a1w1xef9 foreign key (created_by_user_id) references public.evictions_user;
+alter table public.address_entity
+    add constraint FKgorye8dxkkxqkt76vff5mwfpk foreign key (last_modified_by_user_id) references public.evictions_user;
+alter table public.attorney_entity
+    add constraint FKg5p8wpsalo8177xibhofmg0so foreign key (created_by_user_id) references public.evictions_user;
+alter table public.attorney_entity
+    add constraint FK39u5gdi81u52py4j33lc9o25r foreign key (last_modified_by_user_id) references public.evictions_user;
+alter table public.case_entity
+    add constraint FK9os7lxvix0bdf9xs7k969owxl foreign key (created_by_user_id) references public.evictions_user;
+alter table public.case_entity
+    add constraint FKo4pk32c9ebum5vjfra2ipt024 foreign key (last_modified_by_user_id) references public.evictions_user;
+alter table public.case_entity_defendants
+    add constraint FK74ffhi1knvlevihy434ttqw9u foreign key (defendants_id) references public.party_entity;
+alter table public.case_entity_defendants
+    add constraint FK89jbpcgkg8sjmodtywdegw8l4 foreign key (is_defendant_of_id) references public.case_entity;
+alter table public.case_entity_events
+    add constraint FK58poo21r6m9iubjys156reb15 foreign key (events_id) references public.event_entity;
+alter table public.case_entity_events
+    add constraint FKfbv6b9413lpv4xvxpv0fqhhvj foreign key (case_entity_id) references public.case_entity;
+alter table public.case_entity_plaintiffs
+    add constraint FKj4gj04c6jm7ntxhs9yccbqlhk foreign key (plaintiffs_id) references public.party_entity;
+alter table public.case_entity_plaintiffs
+    add constraint FKgob6c5vq2ljo4ubydtuxgxu3b foreign key (is_plaintiff_of_id) references public.case_entity;
+alter table public.event_entity
+    add constraint FKep2o90etyl8tuuqhir8wp47qs foreign key (created_by_user_id) references public.evictions_user;
+alter table public.event_entity
+    add constraint FKtis6v9gx6m48w6vmw5ix0obgm foreign key (last_modified_by_user_id) references public.evictions_user;
+alter table public.party_entity
+    add constraint FK3o4ifiyyy52vbkd6mjuds1ruh foreign key (created_by_user_id) references public.evictions_user;
+alter table public.party_entity
+    add constraint FKbnnr3uf2tr20juepomwqrtv8f foreign key (last_modified_by_user_id) references public.evictions_user;
+alter table public.party_entity
+    add constraint FKqd32hd4df92rwtcfceewu7ro9 foreign key (address_id) references public.address_entity;
+alter table public.party_entity
+    add constraint FKpcb1kvwk2hsa4ik8aw6t0ib3h foreign key (attorney_id) references public.attorney_entity;
