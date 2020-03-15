b35fc2e92daf969b9f9efd3a467284b4fc21c2de
Alvin Gao <gaovinal@gmail.com>
2020-03-14 20:18:14 -0400

    Removed legacy case upload endpoint

--- src/main/java/org/dsacleveland/evictiontracker/controller/rest/CaseController.java
+++ src/main/java/org/dsacleveland/evictiontracker/controller/rest/CaseController.java
@@ -2,18 +2,19 @@ package org.dsacleveland.evictiontracker.controller.rest;
 
 import org.dsacleveland.evictiontracker.model.evictiondata.dto.CaseDto;
 import org.dsacleveland.evictiontracker.model.evictiondata.entity.CaseEntity;
-import org.dsacleveland.evictiontracker.model.evictiondata.legacy.LegacyCase;
 import org.dsacleveland.evictiontracker.service.evictiondata.CaseService;
 import org.dsacleveland.evictiontracker.service.evictiondata.PdfUploadService;
 import org.dsacleveland.evictiontracker.service.util.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -34,11 +35,6 @@ public class CaseController extends AbstractRestController<CaseEntity, CaseDto, 
         this.pdfUploadService = pdfUploadService;
         this.emailSenderService = emailSenderService;
         this.alertEmailAddr = alertEmailAddr;
-    }
-
-    @PostMapping("/upload_legacy")
-    public List<CaseEntity> loadLegacyCases(@RequestBody List<LegacyCase> legacyCases) {
-        return this.entityService.importFromLegacy(legacyCases);
     }
 
     @PostMapping("/upload-pdf")
